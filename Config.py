209d2a7d17ec417b076ddf2eb57f8e7339f1a3ac
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-05-07 04:22:30 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -12,8 +12,8 @@ if ENVIRONMENT:
     DATABASE_URL = os.environ.get('DATABASE_URL', "postgres://zzlrfwmp:75B5tfrr7ocO6MOsfogvSPn2XmZccKIF@chunee.db.elephantsql.com/zzlrfwmp")
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
-    MUST_JOIN = os.environ.get('MUST_JOIN', None)
-    if MUST_JOIN.startswith("bombleebas"):
+    MUST_JOIN = os.environ.get('MUST_JOIN', "bombleebas")
+    if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
     # Fill the Values
